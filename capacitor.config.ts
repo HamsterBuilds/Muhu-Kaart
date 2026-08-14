d1caff9d5658806c23ab5a56287afd4789f19f8b
HamsterBuilds <estlincorporative@gmail.com>
2026-08-14 14:34:38 +0300

    a

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "app.lovable.muhupunktid",
   appName: "Muhu punktid",
-  webDir: "dist/client",
+  webDir: ".output/public",
   plugins: {
     Geolocation: {
       permissions: ["location"],
